516553a6e8eb4050d9b50719e6ce4ce254ac3bf2
DeepakBudhi <119928099+DeepakBudhi@users.noreply.github.com>
2022-12-20 13:10:08 +0530

    Add files via upload

---
+++ QUES2.py
@@ -0,0 +1,9 @@
+#QUESTION 2 :
+name = "Deepak Budhiraja "
+SID = 22105011
+department_name = "Electronics and Communication Engineering"
+CGPA= 9.9
+
+print(f"Hey,{name} Here!\nMy SID is {SID} \nI am from {department_name} department and my CGPA is {CGPA}")
+
+
